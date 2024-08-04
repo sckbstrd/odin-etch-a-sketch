9bafad9e3e0fbf6bb2d0c86ccf530f3f27af5535
sckbstrd <jguitarok@gmail.com>
2024-08-04 22:32:54 +0300

    Add color picker functionality

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const main = document.querySelector('div');
+const main = document.getElementById('main');
+const colorPicker = document.getElementById('colorpicker');
 
 
+// Deep periwinkle and soft lilac
+// Hex codes: #735DA5, #D3C5E5
+// https://webflow.com/blog/best-color-combinations
+
+let defaultColor = '#735DA5';
+
+window.addEventListener("load", startup);
+
+function startup() {
+    colorPicker.value = defaultColor;
+    colorPicker.addEventListener("change", getColor, false);
+    colorPicker.select();
+}
 
 for (let i=0; i<16; i++) {
     const oneRow = document.createElement('div')
@@ -17,9 +31,16 @@ for (let i=0; i<16; i++) {
 
 
 
+function getColor(event) {
+    return colorPicker.value;
+}
+  
 
 main.addEventListener('click', function handleClick(event) {
-    if (event.target.className == 'griddiv'){event.target.style.backgroundColor = 'red';}
-    
+    if (event.target.className == 'griddiv'){event.target.style.backgroundColor=getColor(event);}
+});
 
+main.addEventListener('contextmenu', function handleClick(event) {
+    event.preventDefault();
+    if (event.target.className == 'griddiv'){event.target.style.backgroundColor = 'white';}
 });
