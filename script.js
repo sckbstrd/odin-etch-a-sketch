7e4e05c64c18eadd21765ca38ebb73427f644973
sckbstrd <jguitarok@gmail.com>
2024-08-04 16:54:34 +0300

    Add necessary files, create grid, add hover effect

---
+++ script.js
@@ -0,0 +1,17 @@
+const main = document.querySelector('div');
+
+
+
+for (let i=0; i<16; i++) {
+    const oneRow = document.createElement('div')
+    oneRow.setAttribute('class', 'row');
+    for (let i=0; i<16; i++) {
+        const griddiv = document.createElement('div');
+        griddiv.setAttribute('class', 'griddiv');
+        oneRow.appendChild(griddiv);
+    }
+    main.appendChild(oneRow);
+
+
+}
+
